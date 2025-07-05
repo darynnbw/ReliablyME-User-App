35003482e15455ba50a0a08d60a6881db66d3d47
[dyad] <git@dyad.sh>
2025-07-05 13:44:30 +0300

    [dyad] Simplifying Vite config to fix startup - wrote 1 file(s) + extra files edited outside of Dyad

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react', '@emotion/react', '@emotion/styled', '@mui/icons-material'],
-    include: [
-      '@mui/material', 
-      '@mui/material/utils', 
-      'hoist-non-react-statics',
-      '@mui/x-date-pickers'
-    ]
-  },
 });
