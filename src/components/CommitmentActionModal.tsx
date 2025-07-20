8b1a9e0d2fded7dc850621984ac2524ed8cebf25
[dyad] <git@dyad.sh>
2025-07-20 21:40:55 +0300

    [dyad] Added more space in commitment modal - wrote 1 file(s)

--- src/components/CommitmentActionModal.tsx
+++ src/components/CommitmentActionModal.tsx
@@ -153,7 +153,7 @@ const CommitmentActionModal: React.FC<CommitmentActionModalProps> = ({ open, onC
     >
       {!isSubmitted && (
         <>
-          <DialogTitle sx={{ p: 0, mb: 1 }}>
+          <DialogTitle sx={{ p: 0, mb: 2 }}>
             <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
               <Box>
                 <Typography variant="h5" sx={{ fontWeight: 700, color: '#333', fontSize: '24px', mb: 1 }}>
